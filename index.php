c1faafa09002ebdab8a989dbb7b8e820f21ee480
sled <max-30@mail.ru>
2018-11-04 16:04:07 +0400

    главная форма

--- index.php
+++ index.php
@@ -93,6 +93,6 @@ if(isset($_POST['login']) && isset($_POST['password']))
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="scripts.js"></script>
+    <script src="js/scripts.js"></script>
 </body>
 </html>
