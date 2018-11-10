98bb75fc0a6b10690dc9834c29e9a51828e01adb
sled <max-30@mail.ru>
2018-11-10 23:21:42 +0400

    доработка загрузки продавцов машин

--- parser/avto_exportcsv.php
+++ parser/avto_exportcsv.php
@@ -1,13 +1,44 @@
 <?php
 set_time_limit(0);
-require_once 'config/db.connect.php';
+require_once '../config/db.connect.php';
+
+$list_directori=scandir('/var/www/html/upload',SCANDIR_SORT_NONE);
+foreach ($list_directori as $key)
+{
+  if(!is_dir($key))
+  {
+    $sql_find_file="select id from loadd_file where name='$key'";
+    $db_find_file=mysqli_query($connect, $sql_find_file);
+    $array_find_file=mysqli_fetch_assoc($db_find_file);
+     $count_scan_file=1;
+     
+    if(!isset($array_find_file['id']))
+       {
+         echo $count_scan_file . "   ";
+         if(($count_scan_file % 2)== 0)
+            {
+              $stream=1;
+
+            }
+            else $stream=2;
+            echo $stream ."\n";
+        $sql_insert_file="insert into loadd_file (name, load_info, stream) value('$key', 1, '$stream')";
+        mysqli_query($connect, $sql_insert_file);
+        rename('/var/www/html/upload/'.$key, '/var/www/html/upload/in/'.$key);
+        $count_scan_file++;
+       }
+
+  }
+  // code...
+}
+/*
 $readfile=fopen("1.csv", "r");
 $count=0;
 while (!feof($readfile))
  {
 $date=fgetcsv($readfile, 0, ",");
 
-/*блок сохранения машины*/
+/*блок сохранения машины*//*
 if(!isset($date[3]))
 {
   $date[3]='';
@@ -16,7 +47,7 @@ if(!isset($date[1]))
         {
         $date[1]='';
         }
-/*блок сохранения машины*/
+/*блок сохранения машины*//*
 for($i=0; $i<14; $i++)
         {
          $date[$i]=trim($date[$i]);
@@ -37,7 +68,7 @@ if(!$id_avto_avito)
     $id_avto_avito=mysqli_insert_id($connect);
   }
 /*данные машины сохранил конец блока*/
-/*блок сохранения телефона*/
+/*блок сохранения телефона*//*
 $sql_select_avito_phone="select id from phone where phone_number='$date[13]'";
 $db_select_avito_phone=mysqli_query($connect, $sql_select_avito_phone);
 $select_avito_phone=mysqli_fetch_assoc($db_select_avito_phone);
@@ -49,7 +80,7 @@ $id_phone=$select_avito_phone['id'];
       $id_phone=mysqli_insert_id($connect);
     }
 /*телефон сохранил конец блока*/
-/*блок сохранение города*/
+/*блок сохранение города*//*
 $sql_select_sity="select id from avito_sity where sity_name='$date[12]'";
 $db_select_sity=mysqli_query($connect, $sql_select_sity);
 $select_sity=mysqli_fetch_assoc($db_select_sity);
@@ -61,7 +92,7 @@ if(!$id_sity)
     $id_sity=mysqli_insert_id($connect);
   }
 /*город сохранил конец блока*/
-/*блок сохранения продавца*/
+/*блок сохранения продавца*//*
 $sql_select_avtor="select id from avito_avtor_name where name='$date[11]'";
 $db_select_avtor=mysqli_query($connect, $sql_select_avtor);
 $select_avtor=mysqli_fetch_assoc($db_select_avtor);
@@ -74,14 +105,14 @@ $id_avtor=$select_avtor['id'];
     }
 /*продавца сохранил конец блока*/
 
-/*сохранение основного блока*/
+/*сохранение основного блока*//*
 $sqladdavito="insert into avito_avto value('', '$id_avto_avito', '$date[2]',
  '$date[8]', '$date[9]', '$date[10]', '$id_avtor', '$id_sity', '$id_phone', '$date[14]')";
  mysqli_query($connect, $sqladdavito);
-/*конец основного блока*/
+/*конец основного блока*//*
 $count++;
 echo $count;
 echo "\n";
   }
-fclose($readfile);
+fclose($readfile);*/
  ?>
